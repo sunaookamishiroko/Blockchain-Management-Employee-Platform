7442e91fcf6811e09667f77ab3ca4f739a0417a3
이서윤 <idfn3535@gmail.com>
2021-09-06 17:06:50 +0900

    Feat : 컨트랙트 업데이트
    정보 저장을 위한 개인정보 struct, 사업장 정보 struct, 근로계약서 struct 만듦. 그리고 iteratable하게, 저장하기 쉽게, 정보들을 관리하기 위해서 전역 변수 index를 추가함

--- employerWeb/contracts/LaborConrtract.sol
+++ employerWeb/contracts/LaborConrtract.sol
@@ -3,10 +3,23 @@ pragma solidity ^0.8.0;
 
 contract LaborContract {
   
-  mapping(address => personalInfo) person;
-  mapping(address => workplaceInfo) employerWorkplace;
+  // 사람과 개인정보 mapping
+  mapping(address => personalInfo) private _person;
+
+  // 고용주의 사업장 리스트 index mapping
+  mapping(address => uint256 []) private _employerWorkplaceList;
+
+  // 근로자의 근로계약서 리스트 index mapping
+  mapping(address => uint256 []) private _employeeLaborContractList;
+
+  // 사업장 index 전역 변수
+  uint private _workplaceIndex = 0;
   
-  //identiNumber : 0 => 근로자 1 => 고용주
+  // 근로계약서 index 전역 변수
+  uint private _laborContractIndex = 0;
+  
+  // 사람 개인정보
+  // identiNumber : 0 => 근로자 1 => 고용주
   struct personalInfo {
     uint8 identiNumber;
     string name;
@@ -14,10 +27,27 @@ contract LaborContract {
     string gender;
   }
 
+  // 사업장 정보
   struct workplaceInfo {
     string name;
     string location;
+    address [] employee;
   }
+
+  // 근로계약서 저장소
+  struct laborContract {
+    string ipfsHash;          // ipfsHash
+    string peroid;            // 근로계약기간 
+    string duties;            // 업무내용
+    string workingTime;       // 소정근로시간
+    string workingDays;       // 근무일
+    string wage;              // 임금(시급)
+    string wageday;           // 임금지급일
+    string comment;           // 기타사항
+  }
+
+  
+
 
 
 
