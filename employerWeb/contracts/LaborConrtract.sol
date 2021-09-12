cffbbfc3c8bb60b3fe530b7eeae70473944c8077
이서윤 <idfn3535@gmail.com>
2021-09-13 02:53:37 +0900

    Feat : 출퇴근 기록하는 함수 추가 / attendance 수정

--- employerWeb/contracts/LaborConrtract.sol
+++ employerWeb/contracts/LaborConrtract.sol
@@ -28,22 +28,23 @@ contract LaborContract {
   }
 
   // 사업장 정보
+  // employee와 attendance는 index 번호 같아야함 -> 검색 쉽게 하기 위함
   struct workplaceInfo {
     string name;
     string location;
     address [] employee;
-    attendance [] attendance;
+    attendance [] attendanceList;
   }
 
-  // 출석부 // 후에 프론트 화면에서 요구하는 양식에 따라 변경할 수 있음
+  // 출톼근 기록부
+  // 후에 프론트 화면에서 요구하는 양식에 따라 변경할 수 있음
   struct attendance {
-    address employee;
-    string startDay;
-    uint startTimeHour;
-    uint startTimeMinute;
-    string endDay;
-    uint endTimeHour;
-    uint endTimeMinute;
+    string [] startDay;
+    uint [] startTimeHour;
+    uint [] startTimeMinute;
+    string [] endDay;
+    uint [] endTimeHour;
+    uint [] endTimeMinute;
   }
 
   // 근로계약서 저장소
@@ -56,8 +57,47 @@ contract LaborContract {
     string wageday;           // 임금지급일
     string comment;           // 기타사항
   }
+  
+  //struct 배열 선언부
 
+  personalInfo [] personalinfo;
+  workplaceInfo [] workplaceinfo;
+  laborContract [] laborcontract;
 
+  // 출퇴근 올리는 함수
+  // classifyNum : 0 -> 출근 / 1 -> 퇴근
+  function uploadAttendance (uint8 classifyNum, string memory day, uint timeHour, 
+  uint timeMinute, uint workPlaceInfoIndex) public returns (uint8) {
 
+    require(workPlaceInfoIndex < workplaceinfo.length, "your workplace is not available");
+
+    address employee = address(0);
+    uint employeeIndex;
+    
+    for (employeeIndex = 0 ; employeeIndex  <= workplaceinfo[workPlaceInfoIndex].employee.length ; employeeIndex++) {
+      if (workplaceinfo[workPlaceInfoIndex].employee[employeeIndex] == msg.sender) {
+        employee = msg.sender;
+      }
+    }
+
+    require(employee != address(0), "your address is not employee");
+
+    if (classifyNum == 0) {
+
+      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startDay.push(day);
+      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startTimeHour.push(timeHour);
+      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startTimeMinute.push(timeMinute);
+      
+    } else if (classifyNum == 1){
+
+      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endDay.push(day);
+      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endTimeHour.push(timeHour);
+      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endTimeMinute.push(timeMinute);
+
+    }
+
+    return 1;
+
+  }
 
 }
