9b48bfe5795ae76251a29f9e5fa7e5dc778518a9
이서윤 <idfn3535@gmail.com>
2021-09-15 17:42:10 +0900

    Feat : 라이브러리 import

--- employeeApp/App.js
+++ employeeApp/App.js
@@ -2,6 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Import the crypto getRandomValues shim (**BEFORE** the shims)
+import "react-native-get-random-values"
+
+// Import the the ethers shims (**BEFORE** ethers)
+import "@ethersproject/shims"
+
+// Import the ethers library
+import { ethers } from "ethers";
+
 export default function App() {
   return (
     <View style={styles.container}>
