41842573fea5ff1c79174dd6d7c25ced5d457bf5
이서윤 <idfn3535@gmail.com>
2021-10-27 23:40:29 +0900

    Refactor : 컨트랙트 충돌 병합중

--- employerWeb/contracts/test.sol
+++ employerWeb/contracts/test.sol
@@ -9,12 +9,6 @@ contract LaborContract {
   // 고용주의 사업장 리스트 index mapping
   mapping(address => uint256 []) private _employerWorkplaceList;
 
-<<<<<<< Updated upstream
-  // 근로자의 근무지 리스트 index mapping
-  mapping(address => uint256 []) private _employeeWorkplaceList;
-
-=======
->>>>>>> Stashed changes
   // 근로자의 근로계약서 리스트 index mapping
   mapping(address => uint256 []) private _employeeLaborContractList;
 
@@ -31,40 +25,12 @@ contract LaborContract {
     string name;
     uint age;
     string gender;
-<<<<<<< Updated upstream
-=======
     string residence;
->>>>>>> Stashed changes
   }
 
   // 사업장 정보
   // employee와 attendance는 index 번호 같아야함 -> 검색 쉽게 하기 위함
   struct workplaceInfo {
-<<<<<<< Updated upstream
-    string name;
-    string location;
-    address [] employee;
-    attendance [] attendanceList;
-    laborContract [] laborcontract;
-    personalInfo [] personalInformation;
-  }
-
-  // 출톼근 기록부
-  // 후에 프론트 화면에서 요구하는 양식에 따라 변경할 수 있음
-  struct attendance {
-    //string [] startDay;
-    uint startYear;
-    uint startMonth;
-    uint startDate;
-    uint startTimeHour;
-    uint startTimeMinute;
-    //string [] endDay;
-    uint endYear;
-    uint endMonth;
-    uint endDate;
-    uint endTimeHour;
-    uint endTimeMinute;
-=======
     string employerName;
     string workplaceName;
     string name;
@@ -89,7 +55,6 @@ contract LaborContract {
     uint [] endDate;
     uint [] endTimeHour;
     uint [] endTimeMinute;
->>>>>>> Stashed changes
   }
 
   //해당 월 출근 기록부
@@ -101,11 +66,6 @@ contract LaborContract {
 
   // 근로계약서 저장소
   struct laborContract {
-<<<<<<< Updated upstream
-    string peroid;            // 근로계약기간 
-    string duties;            // 업무내용
-    string workingTime;       // 소정근로시간
-=======
     address employer;
     address employee;
     string employeeName;
@@ -114,8 +74,7 @@ contract LaborContract {
     string duties;            // 업무내용
     string workingStartTime;
     string workingTime;
-    string workingCycle       // 소정근로시간
->>>>>>> Stashed changes
+    string workingCycle;       // 소정근로시간
     string workingDays;       // 근무일
     string wage;              // 임금(시급)
     string wageday;           // 임금지급일
@@ -123,43 +82,6 @@ contract LaborContract {
   }
   
   //struct 배열 선언부
-<<<<<<< Updated upstream
-  personalInfo [] personalinfo;
-  workplaceInfo [] workplaceinfo;
-  laborContract [] laborcontract;
-
-  //사람 개인정보 추가
-  function addPeopleInfo(address ethAddress, uint8 identiNumber, string name, uint age, string gender) public {
-    _person[ethAddress].identiNumber = identiNumber;
-    _person[ethAddress].name = name;
-    _person[ethAddress].age = age;
-    _person[ethAddress].gender = gender;
-  }
-
-  //근로자의 근무지들 조회하는 함수
-  function checkWorkplace (address employeeAddress) public returns (uint [], string [], string []){
-    
-    uint [] array = _employeeWorkplaceList[employeeAddress];
-    string [] name;
-    string [] location;
-    
-    for (uint x = 0; x < array.length ; x++) {
-      name.push(workplaceinfo[array[x]].name);
-      location.push(workplaceinfo[array[x]].location);
-    }
-
-    return (array, name, location);
-
-  }
-
-
-  //근로자의 근로계약서를 조회하는 함수
-  //아직 미완성 입니다.(근로계약서 등록을 어떻게 하냐에 따라 조회 방법이 달라질 듯 합니다.)
-  function checkLaborContract (uint workPlaceInfoIndex, address employeeAddress) public view
-  returns(string _period, string _duties, string _workingTime, string _workingDays, string _wage, string _wageday, string _comment) {
-
-    require(workPlaceInfoIndex < workplaceinfo.length, "workplace is not available");
-=======
 
   personalInfo [] personalinfo;
   workplaceInfo [] workplaceinfo;
@@ -321,288 +243,3 @@ contract LaborContract {
     }
     return 1;
   }
-
-  //근로자의 근무지 조회하는 함수
-  // 후에 프론트 화면에서 요구하는 양식에 따라 변경할 수 있음
-  function checkWorkplace (address employeeAddress, uint workPlaceInfoIndex ) public returns (string _name, string _location){
-  
-    require(workPlaceInfoIndex < workplaceinfo.length, "your workplace is not available");
->>>>>>> Stashed changes
-
-    address employee = address(0);
-    uint employeeIndex;
-    
-<<<<<<< Updated upstream
-    for (employeeIndex = 0 ; employeeIndex  < workplaceinfo[workPlaceInfoIndex].employee.length ; employeeIndex++) {
-=======
-    for (employeeIndex = 0 ; employeeIndex  <= workplaceinfo[workPlaceInfoIndex].employee.length ; employeeIndex++) {
->>>>>>> Stashed changes
-      if (workplaceinfo[workPlaceInfoIndex].employee[employeeIndex] == employeeAddress) {
-        employee = employeeAddress;
-        break;
-      }
-    }
-
-<<<<<<< Updated upstream
-    return (
-      workplaceinfo[workPlaceInfoIndex].laborcontract[employeeIndex].period,
-      workplaceinfo[workPlaceInfoIndex].laborcontract[employeeIndex].duties,
-      workplaceinfo[workPlaceInfoIndex].laborcontract[employeeIndex].workingTime,
-      workplaceinfo[workPlaceInfoIndex].laborcontract[employeeIndex].workingDays,
-      workplaceinfo[workPlaceInfoIndex].laborcontract[employeeIndex].wage,
-      workplaceinfo[workPlaceInfoIndex].laborcontract[employeeIndex].wageday,
-      workplaceinfo[workPlaceInfoIndex].laborcontract[employeeIndex].comment);
-=======
-    require(employee != address(0), "you are not employee");
-
-    return (
-      workplaceinfo[workPlaceInfoIndex].name,
-      workplaceinfo[workPlaceInfoIndex].location);
-
-  }
-
-
-  //근로자의 근로계약서를 조회하는 함수
-  //아직 미완성 입니다.(근로계약서 등록을 어떻게 하냐에 따라 조회 방법이 달라질 듯 합니다.)
-  function checkLaborContract (address employeeAddress) public view
-  returns(string _period, string _duties, string _workingTime, string _workingDays, string _wage, string _wageday, string _comment) {
-
-    //require문에 본인의 어드레스랑 근로계약서의 어드레스 비교하는 과정 넣으면 됨(for문써서)
-
-    return (
-      laborcontract[].period,
-      laborcontract[].duties,
-      laborcontract[].workingTime,
-      laborcontract[].workingDays,
-      laborcontract[].wage,
-      laborcontract[].wageday,
-      laborcontract[].comment);
->>>>>>> Stashed changes
-
-  }
-
-  //당일 근로자가 일한 시간 조회하는 함수
-  //hour(시간), minute(분)을 숫자로 출력합니다   -> 몇 시간 몇 분 근무했는지에 대한 정보
-  function checkWorkTime (address employeeAddress, uint workPlaceInfoIndex) public returns (uint _hour, uint _minute) {
-
-    require(workPlaceInfoIndex < workplaceinfo.length, "error");
-
-    address employee = address(0);
-    uint employeeIndex;
-    
-<<<<<<< Updated upstream
-    for (employeeIndex = 0 ; employeeIndex  < workplaceinfo[workPlaceInfoIndex].employee.length ; employeeIndex++) {
-=======
-    for (employeeIndex = 0 ; employeeIndex  <= workplaceinfo[workPlaceInfoIndex].employee.length ; employeeIndex++) {
->>>>>>> Stashed changes
-      if (workplaceinfo[workPlaceInfoIndex].employee[employeeIndex] == employeeAddress) {
-        employee = employeeAddress;
-        break;
-      }
-    }
-
-    require(employee != address(0), "you are not employee");
-
-    uint date = 1;                                              //추우에 날짜 기입 방법이 확정될 경우 이용될 수 있음
-    uint hour = workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endTimeHour - workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startTimeHour;
-    uint minute = workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endTimeMinute - workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startTimeMinute;
-
-    if(hour < 0) {
-      hour = hour + 24;
-    }
-
-    if (minute < 0) {
-      hour = hour - 1;
-      minute = minute + 60;
-    }
-
-    return (hour, minute);
-
-  }
-
-
-
-  //근로자의 급여 조회
-<<<<<<< Updated upstream
-  function checkPayment (address employeeAddress, uint workPlaceInfoIndex, uint workedYear, uint workedMonth, uint wage) public 
-  returns (uint) {
-
-    require(workPlaceInfoIndex < workplaceinfo.length, "error");
-
-    address employee = address(0);
-    uint employeeIndex;
-    uint Index = 0;
-    uint startIndex;
-    uint endIndex;
-    uint hour;
-    uint minute;
-    uint dailyWage;
-    uint montlyWage = 0;
-    
-    for (employeeIndex = 0 ; employeeIndex  < workplaceinfo[workPlaceInfoIndex].employee.length ; employeeIndex++) {
-      if (workplaceinfo[workPlaceInfoIndex].employee[employeeIndex] == employeeAddress) {
-        employee = employeeAddress;
-        break;
-      }
-    }
-
-    require(employee != address(0), "you are not employee");
-    
-    //출근기록부를 바탕으로 해당 년, 월에 출근한 내역을 뽑아낸다
-    //조회를 원하는 년도의 데이터를 갖고 있는 배열의 인덱스 번호를 찾는다
-    while(workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startYear[Index] != workedYear){
-      Index = Index + 1;
-    }
-
-    //이어서 조회를 원하는 월의 데이터를 갖고 있는 배열의 인덱스 번호를 찾는다
-    while(workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startMonth[Index] != workedMonth){
-      Index = Index + 1;
-    }
-    //해당 인덱스 번호부터 조회를 원하는 월의 데이터가 아닌 배열이 나타날때까지 다시 조회하여 인덱스 번호를 찾는다
-    startIndex = Index;
-
-    while(workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startMonth[Index] == workedMonth){
-       Index = Index + 1;
-    }
-    //조회를 원하는 월의 데이터를 벗어난 시점에서의 인덱스 번호를 마지막 인덱스로 한다
-    endIndex = Index;
-    //조회하고자 하는 년도, 월의 값을 모두 갖고 있는 인덱스부터 시작하여, 다음달 값이 들어가는 인덱스까지 반복
-    for(startIndex; startIndex <endIndex; startIndex++){
-      hour = workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endTimeHour[startIndex] - workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startTimeHour[startIndex];
-      minute = workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endTimeMinute[startIndex] - workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startTimeMinute[startIndex];
-
-      if(hour < 0) {
-        hour = hour + 24;
-      }
-
-      if (minute < 0) {
-        hour = hour - 1;
-        minute = minute + 60;
-      }
-      dailyWage = (wage/60)*(hour*60+minute);
-      montlyWage = monthlyWage + dailyWage;
-
-    }
-
-    return (montlyWage);
-  }
-
-  //근로자의 개인 정보 조회하는 함수
-  function checkEmployeeInformation (address employeeAddress, uint workPlaceInfoIndex ) public view
-  returns (address _employee, uint8 _identiNumber, string _name, uint _age, string gender){
-  
-    require(workPlaceInfoIndex < workplaceinfo.length, "your workplace is not available");
-
-    address employee = address(0);
-    uint employeeIndex;
-    
-    for (employeeIndex = 0 ; employeeIndex  < workplaceinfo[workPlaceInfoIndex].employee.length ; employeeIndex++) {
-      if (workplaceinfo[workPlaceInfoIndex].employee[employeeIndex] == employeeAddress) {
-        employee = employeeAddress;
-        break;
-      }
-    }
-
-    require(employee != address(0), "you are not employee");
-
-    return (
-      workplaceinfo[workPlaceInfoIndex].employee[employeeIndex],
-      workplaceinfo[workPlaceInfoIndex].personalInformation[employeeIndex].identiNumber,
-      workplaceinfo[workPlaceInfoIndex].personalInformation[employeeIndex].name,
-      workplaceinfo[workPlaceInfoIndex].personalInformation[employeeIndex].age,
-      workplaceinfo[workPlaceInfoIndex].personalInformation[employeeIndex].gender);
-
-  }
-=======
-  function checkPayment (address employeeAddress) public returns ( ) {
->>>>>>> Stashed changes
-
-
-  }
-
-  // 출퇴근 올리는 함수
-  // classifyNum : 0 -> 출근 / 1 -> 퇴근
-  // 날짜 입력 형식을 기존 string 형식에서 uint 년도, uint 몇월, uint 몇일 형식으로 변경하였습니다.
-  function uploadAttendance (uint8 classifyNum, uint year, uint month, uint date,
-  uint timeHour, uint timeMinute, uint workPlaceInfoIndex) public returns (uint8) {
-
-    require(workPlaceInfoIndex < workplaceinfo.length, "your workplace is not available");
-
-    address employee = address(0);
-    uint employeeIndex;
-    
-<<<<<<< Updated upstream
-    for (employeeIndex = 0 ; employeeIndex  < workplaceinfo[workPlaceInfoIndex].employee.length ; employeeIndex++) {
-=======
-    for (employeeIndex = 0 ; employeeIndex  <= workplaceinfo[workPlaceInfoIndex].employee.length ; employeeIndex++) {
->>>>>>> Stashed changes
-      if (workplaceinfo[workPlaceInfoIndex].employee[employeeIndex] == msg.sender) {
-        employee = msg.sender;
-        break;
-      }
-    }
-
-    require(employee != address(0), "you are not employee");
-
-    if (classifyNum == 0) {
-
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startYear.push(year);
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startMonth.push(month);
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startDate.push(date);
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startTimeHour.push(timeHour);
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startTimeMinute.push(timeMinute);
-      
-    } else if (classifyNum == 1){
-
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endYear.push(year);
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endMonth.push(month);
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endDate.push(date);
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endTimeHour.push(timeHour);
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endTimeMinute.push(timeMinute);
-
-    }
-
-    return 1;
-
-  }
-
-  // 출퇴근 내역을 return하는 함수
-  // 후에 프론트 화면에서 요구하는 양식에 따라 변경할 수 있음
-  // 출퇴근 출력 내용 중 날짜 부분을 년도, 월, 일 세분화하였습니다.
-  function checkAttendance (uint workPlaceInfoIndex, address employeeAddress) public view 
-  returns (uint [] memory, uint [] memory, uint [] memory, uint [] memory, uint [] memory, 
-  uint [] memory, uint [] memory, uint [] memory, uint [] memory, uint [] memory){
-
-    require(workPlaceInfoIndex < workplaceinfo.length, "workplace is not available");
-
-    address employee = address(0);
-    uint employeeIndex;
-    
-<<<<<<< Updated upstream
-    for (employeeIndex = 0 ; employeeIndex  < workplaceinfo[workPlaceInfoIndex].employee.length ; employeeIndex++) {
-=======
-    for (employeeIndex = 0 ; employeeIndex  <= workplaceinfo[workPlaceInfoIndex].employee.length ; employeeIndex++) {
->>>>>>> Stashed changes
-      if (workplaceinfo[workPlaceInfoIndex].employee[employeeIndex] == employeeAddress) {
-        employee = employeeAddress;
-        break;
-      }
-    }
-
-    require(employee != address(0), "you are not employee");
-
-    return (
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startYear,
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startMonth,
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startDate,
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startTimeHour,
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].startTimeMinute,
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endYear,
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endMonth,
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endDate,
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endTimeHour,
-      workplaceinfo[workPlaceInfoIndex].attendanceList[employeeIndex].endTimeMinute );
-  }
-
-
-}
