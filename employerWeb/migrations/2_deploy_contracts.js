fc3abf9ff35bcd1d934e5910f5a6929b02cc0bab
이서윤 <idfn3535@gmail.com>
2022-01-05 16:06:16 +0900

    Refactor : sample과 우리가 쓸 app.js 나눔

--- employerWeb/migrations/2_deploy_contracts.js
+++ employerWeb/migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ var ERC20 = artifacts.require("./ERC20.sol");
 var Labor = artifacts.require("./LaborContract.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ERC20,"money", "WON");
+  deployer.deploy(ERC20,"Money", "WON");
   deployer.deploy(Labor);
 };
