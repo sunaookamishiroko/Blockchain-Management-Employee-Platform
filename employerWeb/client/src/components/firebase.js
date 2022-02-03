0522a5ad398e40f88c4526bfb95b69dbe9956921
Taemin0624 <82110225+Taemin0624@users.noreply.github.com>
2022-02-03 21:50:40 +0900

    Feat : firestore config 삭제

--- employerWeb/client/src/components/firebase.js
+++ employerWeb/client/src/components/firebase.js
@@ -4,13 +4,7 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCtv1d2ebcDVJ6OQcArZP7imTe6V7nI5UQ",
-    authDomain: "blockchain-management.firebaseapp.com",
-    projectId: "blockchain-management",
-    storageBucket: "blockchain-management.appspot.com",
-    messagingSenderId: "1087770585080",
-    appId: "1:1087770585080:web:73c38d94fe24ecceed980d",
-    measurementId: "G-RQKSD5XC2F"
+    
   };
 
 firebase.initializeApp(firebaseConfig);
