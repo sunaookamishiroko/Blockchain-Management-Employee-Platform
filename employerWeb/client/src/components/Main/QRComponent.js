7964cf10390534aad2de18dbc1c20429ceebf817
qpdh <74190329+qpdh@users.noreply.github.com>
2022-03-15 23:21:04 +0900

    Feat : QRCode 추가
    TODO 근로자 주소, 난수값 전달해야 함

--- employerWeb/client/src/components/Main/QRComponent.js
+++ employerWeb/client/src/components/Main/QRComponent.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-
+import QRCode from 'qrcode.react';
 /*
 QR 코드를 보여주는 컴포넌트 입니다.
-
 */
 
 const StyledQRComponent = styled.div`
+display:flex;
+align-items:center;
+flex-direction:column;
   width: 294px;
   height: 300px;
   box-shadow: 5px 5px 5px 5px gray;
@@ -16,9 +18,15 @@ const StyledQRComponent = styled.div`
 `;
 
 const QRComponent = () => {
+  
   return (
     <StyledQRComponent>
       <h2 style={{ textAlign: "center" }}>QR 코드 인증</h2>
+      {/* value에 url을 쏴줄 것
+      string 값도 가능
+      
+       */}
+      <QRCode value="test"/>
     </StyledQRComponent>
   );
 };
