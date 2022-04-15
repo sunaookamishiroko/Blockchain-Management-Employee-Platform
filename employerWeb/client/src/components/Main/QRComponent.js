5244249eab969ff2e1e41edb5deafffda7086dcc
이서윤 <idfn3535@gmail.com>
2022-04-15 19:39:54 +0900

    Fix : 1 ~ 1억 사이의 랜덤한 수 나오도록 수정

--- employerWeb/client/src/components/Main/QRComponent.js
+++ employerWeb/client/src/components/Main/QRComponent.js
@@ -18,7 +18,7 @@ const StyledQRComponent = styled.div`
 `;
 
 const QRComponent = () => {
-  const QRValue = Math.random(1, 100);
+  const QRValue = Math.floor(Math.random() * 100000000) + 1;
   return (
     <StyledQRComponent>
       <h2 style={{ textAlign: "center" }}>QR 코드 인증</h2>
