5c3fd5555b8cb46849854c700aef69d42caf5554
이서윤 <idfn3535@gmail.com>
2022-05-03 01:41:49 +0900

    Feat: controller 추가

---
+++ database-backend/src/main/java/com/example/databasebackend/laborcontract/LaborContractController.java
@@ -0,0 +1,13 @@
+package com.example.databasebackend.laborcontract;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LaborContractController {
+
+    @GetMapping("/getcontract")
+
+    @PostMapping("/setcontract")
+}
