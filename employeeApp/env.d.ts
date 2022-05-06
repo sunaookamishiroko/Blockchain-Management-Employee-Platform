1b3512ae310225e8a1d9d947662ea05d7eab42db
이서윤 <idfn3535@gmail.com>
2022-05-06 22:35:05 +0900

    Feat: pinata api key 추가

--- employeeApp/env.d.ts
+++ employeeApp/env.d.ts
@@ -1,5 +1,7 @@
 declare module '@env' {
     export const PROVIDER_APIKEY: string;
+    export const PINATA_APIKEY: string;
+    export const PANATA_SECRET_APIKEY: string;
     export const LABORCONTRACT_ADDRESS: string;
     export const ERC20_ADDRESS: string;
     export const MYNFT_ADDRESS: string;
