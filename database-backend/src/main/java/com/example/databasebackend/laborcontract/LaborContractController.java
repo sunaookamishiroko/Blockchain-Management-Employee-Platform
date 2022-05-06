073d9a3e6423ddba8ab5c2b103c3cf375945ee2c
이서윤 <idfn3535@gmail.com>
2022-05-06 20:41:48 +0900

    Feat : backend 기본 뼈대

--- database-backend/src/main/java/com/example/databasebackend/laborcontract/LaborContractController.java
+++ database-backend/src/main/java/com/example/databasebackend/laborcontract/LaborContractController.java
@@ -2,12 +2,19 @@ package com.example.databasebackend.laborcontract;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LaborContractController {
 
     @GetMapping("/getcontract")
+    public void getContract() {
+
+    }
 
     @PostMapping("/setcontract")
+    pulbic void setContract(@RequestBody ) {
+
+    }
 }
