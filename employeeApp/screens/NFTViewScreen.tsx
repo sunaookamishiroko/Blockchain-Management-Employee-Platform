5392284b135cf0da4601af215fd01509ed25a59f
qpdh <74190329+qpdh@users.noreply.github.com>
2022-05-29 22:01:49 +0900

    done: 나의 보상 상세 보기 css

--- employeeApp/screens/NFTViewScreen.tsx
+++ employeeApp/screens/NFTViewScreen.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import { StyleSheet, Image } from 'react-native';
+import React from "react";
+import { StyleSheet, Image } from "react-native";
 
-import { Text, View } from '../components/Themed';
-import { styles } from '../css/styles';
+import { Text, View } from "../components/Themed";
+import { styles } from "../css/styles";
 
 // NFT 자세히 보기
 export default function NFTViewScreen({ route }) {
-
   return (
     <View style={styles.container}>
-      <Image 
-        source={{uri: route.params.tokeninfo.metadata.image}}
-        style={{width: 250, height: 250}}
+      <Image
+        source={{ uri: route.params.tokeninfo.metadata.image }}
+        style={{
+          width: 250,
+          height: 250,
+          resizeMode: "contain",
+          marginBottom: 40,
+        }}
       />
-      <Text>{route.params.tokeninfo.metadata.name}</Text>
-      <Text>설명 : {route.params.tokeninfo.metadata.description}</Text>
-      <Text>nftindex : {route.params.tokeninfo.metadata.nftindex}</Text>
-      <Text>{route.params.tokeninfo.wpinfo.wpname}</Text>
-      <Text>{route.params.tokeninfo.wpinfo.wplocation}</Text>
+      <View style={{}}>
+        <Text>NFT : {route.params.tokeninfo.metadata.name}</Text>
+        <Text>설명 : {route.params.tokeninfo.metadata.description}</Text>
+        {/* <Text>nftindex : {route.params.tokeninfo.metadata.nftindex}</Text> */}
+        <Text>발급 지점 : {route.params.tokeninfo.wpinfo.wpname}</Text>
+        <Text>지점 주소 : {route.params.tokeninfo.wpinfo.wplocation}</Text>
+      </View>
     </View>
   );
 }
