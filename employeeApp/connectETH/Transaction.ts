ab39497470a912cfb023f544dc757747cfeb55e4
이서윤 <idfn3535@gmail.com>
2022-05-31 19:46:31 +0900

    Refactor : wontoken 관련 함수 추가, 설정

--- employeeApp/connectETH/Transaction.ts
+++ employeeApp/connectETH/Transaction.ts
@@ -3,6 +3,7 @@ import { PROVIDER_APIKEY, LABORCONTRACT_ADDRESS, ERC20_ADDRESS, MYNFT_ADDRESS, W
 import LaborContractABI from "../contracts/LaborContract.json";
 import ERC20ContractABI from "../contracts/ERC20.json";
 import NFTContractABI from "../contracts/myNFT.json";
+import WonTokenContractABI from "../contracts/WonToken.json";
 
 import "react-native-get-random-values";
 import "@ethersproject/shims";
@@ -13,6 +14,7 @@ export const infuraProvider = new ethers.providers.InfuraProvider("ropsten", PRO
 export const laborContract = new ethers.Contract(LABORCONTRACT_ADDRESS, LaborContractABI.abi, infuraProvider);
 export const ERC20Contract = new ethers.Contract(ERC20_ADDRESS, ERC20ContractABI.abi, infuraProvider);
 export const NFTContract = new ethers.Contract(MYNFT_ADDRESS, NFTContractABI.abi, infuraProvider);
+export const WonTokenContract = new ethers.Contract(WONTOKEN_ADDRESS, WonTokenContractABI.abi, infuraProvider);
 
 export async function makeLabortxobj(from: string, abidata: string, gaslimit: number) {
 
@@ -31,7 +33,7 @@ export async function makeLabortxobj(from: string, abidata: string, gaslimit: nu
   return txObj;
 }
 
-export async function makeTokentxobj(from: string, abidata: string, gaslimit: number) {
+export async function makeWonTokentxobj(from: string, abidata: string, gaslimit: number, value: number) {
 
   let nonce = await infuraProvider.getTransactionCount(from, "latest");
   let gasfee = (await infuraProvider.getGasPrice())._hex;
@@ -42,25 +44,8 @@ export async function makeTokentxobj(from: string, abidata: string, gaslimit: nu
       gas: gaslimit,
       gasPrice: gasfee,
       nonce: nonce,
-      to: ERC20_ADDRESS,
-      value: "0x00"
-  };
-  return txObj;
-}
-
-export async function makeNfttxobj(from: string, abidata: string, gaslimit: number) {
-
-  let nonce = await infuraProvider.getTransactionCount(from, "latest");
-  let gasfee = (await infuraProvider.getGasPrice())._hex;
-
-  let txObj = {
-      data: abidata,
-      from: from,
-      gas: gaslimit,
-      gasPrice: gasfee,
-      nonce: nonce,
-      to: MYNFT_ADDRESS,
-      value: "0x00"
+      to: WONTOKEN_ADDRESS,
+      value: value
   };
   return txObj;
 }
--- employeeApp/screens/TabFourScreen.tsx
+++ employeeApp/screens/TabFourScreen.tsx
@@ -14,6 +14,7 @@ import { ethers } from "ethers";
 
 import {
   makeLabortxobj,
+  makeWonTokentxobj,
   laborContract,
   ERC20Contract,
 } from "../connectETH/Transaction";
