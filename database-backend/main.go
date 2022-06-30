0c5184e50310ec9126cd1907d052366d7a2e9d34
이서윤 <idfn3535@gmail.com>
2022-06-30 17:09:37 +0900

    Feat : 핸들러 등록

--- database-backend/main.go
+++ database-backend/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	handler := gin.Default()
+	handler.GET("/test", test)
+
+	handler.GET("/qrcode", getQRcode)
+	handler.POST("/qrcode", setQRcode)
+
+	handler.GET("/contract", getLaborContract)
+	handler.POST("/contract", setLaborContract)
+	handler.DELETE("/contract", deleteLaborContract)
+
+	handler.Run()
+}
+
+func getQRcode(c *gin.Context) {
+
+}
+
+func setQRcode(c *gin.Context) {
+
+}
+
+func getLaborContract(c *gin.Context) {
+
+}
+
+func setLaborContract(c *gin.Context) {
+
+}
+
+func deleteLaborContract(c *gin.Context) {
+
+}
+
+func test(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "testok",
+	})
 }
