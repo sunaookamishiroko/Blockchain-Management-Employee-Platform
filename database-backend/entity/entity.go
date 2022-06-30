a2bdef9a106efaa5fa9830308a3e1d44d9846443
이서윤 <idfn3535@gmail.com>
2022-06-30 17:34:42 +0900

    Feat : database entity 추가

---
+++ database-backend/entity/entity.go
@@ -0,0 +1,21 @@
+package entity
+
+type Qrcode struct {
+	WorkplaceIndex int    `json:"workplaceindex"`
+	Date           string `json:"date"`
+	RandomNum      int    `json:"randomnum"`
+}
+
+type LaborContract struct {
+	Address        string `json:"address"`
+	WpName         string `json:"wpname"`
+	WpEmployer     string `json:"wpemployer"`
+	EmployeeName   string `json:"employeename"`
+	WorkplaceIndex int    `json:"workplaceindex"`
+	Period         string `json:"period"`
+	Duties         string `json:"duties"`
+	WorkingTime    string `json:"workingtime"`
+	WorkingDays    string `json:"workingdays"`
+	Wage           string `json:"wage"`
+	WageDay        string `json:"wageday"`
+}
