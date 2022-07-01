fa670f7ee06e7014f60688fe71f2152dc16745c6
이서윤 <idfn3535@gmail.com>
2022-07-01 21:10:06 +0900

    Feat : 여러 세팅 추가

--- employerWeb/client/src/envSetting.js
+++ employerWeb/client/src/envSetting.js
@@ -1,3 +1,17 @@
-export const ENDPOINT = ``;
+// DB endpoint
+export const ENDPOINT = `http://localhost:8080/`;
+
+// pinata setting
 export const PINATA_API_KEY = ``;
 export const PINATA_SECRET_API_KEY = ``;
+
+// NFT 뱃지 이미지 세팅
+// pinata ifpshash 필요
+export const BADGE1 = ``;
+export const BADGE2 = ``;
+export const BADGE3 = ``;
+
+// TODAY : 오늘 날짜
+export const TODAY = `2022-07-01`;
+// DELETEDAY : 근로자 근로 계약 해지하는 날짜 -> 근로자 경력에 입력됨
+export const DELETEDAY = `2022-08-31`
