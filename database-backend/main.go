16e8e6d28ababf2808160bd8673a4bcfeb2ee67c
이서윤 <idfn3535@gmail.com>
2022-09-06 21:05:58 +0900

    Fix : 구조체 필드에 json binding이 안되던 문제 수정

--- database-backend/main.go
+++ database-backend/main.go
@@ -183,12 +183,12 @@ func setLaborContract(c *gin.Context) {
 // ex) /contract
 // 입력은 json
 func deleteLaborContract(c *gin.Context) {
-	var info struct {
-		address        string
-		workplaceindex int
+	var res struct {
+		Address        string
+		Workplaceindex int
 	}
 
-	err := c.BindJSON(&info)
+	err := c.BindJSON(&res)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, errorResponse(err))
 		log.Print(err.Error())
@@ -202,7 +202,7 @@ func deleteLaborContract(c *gin.Context) {
 	}
 
 	_, err = db.Exec("DELETE FROM laborcontract WHERE address= ? AND workplaceindex= ?",
-		info.address, info.workplaceindex)
+		res.Address, res.Workplaceindex)
 
 	if err != nil {
 		c.JSON(http.StatusBadRequest, errorResponse(err))
