85fd49c5a7f9ef7fbc6ad9b3c0007fc19479b5c7
이서윤 <idfn3535@gmail.com>
2022-09-06 16:32:36 +0900

    Feat : qrcode 테스트 코드 추가

--- database-backend/main_test.go
+++ database-backend/main_test.go
@@ -1,39 +1,79 @@
 package main
 
 import (
+	"bytes"
+	"database-backend/entity"
 	"database/sql"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestGetQRcode(t *testing.T) {
+func TestSetQRcode(t *testing.T) {
+	// before
 	router := gin.Default()
-	router.GET("/qrcode", getQRcode)
+	router.POST("/qrcode", setQRcode)
+
+	qr := entity.Qrcode{
+		WorkplaceIndex: 0,
+		Date:           "2022-09-07",
+		RandomNum:      1234,
+	}
+	body, _ := json.Marshal(qr)
+
+	// do
+	req, _ := http.NewRequest("POST", "/qrocde", bytes.NewBuffer(body))
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	// after
 	assert := assert.New(t)
 
-	db, err := sql.Open("mysql", "root:rootroot@tcp(127.0.0.1:3306)/capstone")
+	db, _ := sql.Open("mysql", "root:rootroot@tcp(127.0.0.1:3306)/capstone")
 	defer db.Close()
-	if err != nil {
-		panic(err)
-	}
-	_, err = db.Exec("INSERT INTO qrcodecheck VALUES (?, ?, ?)", 0, "2022-12-01", 123456)
-	if err != nil {
-		t.Error()
-	}
 
+	db.QueryRow("SELECT * FROM qrcode WHERE workplaceindex= ? AND date= ?",
+		0, "2022-09-07").Scan(&qr.WorkplaceIndex, &qr.Date, &qr.RandomNum)
+
+	assert.Equal(0, qr.WorkplaceIndex)
+	assert.Equal("2022-09-07", qr.Date)
+	assert.Equal(1234, qr.RandomNum)
 }
 
-func TestSetQRcode(t *testing.T) {
+func TestGetQRcode(t *testing.T) {
+	// before
+	router := gin.Default()
+	router.GET("/qrcode", getQRcode)
+
+	db, _ := sql.Open("mysql", "root:rootroot@tcp(127.0.0.1:3306)/capstone")
+	defer db.Close()
+
+	db.Exec("INSERT INTO qrcode VALUES (?, ?, ?)", 0, "2022-09-07", 1234)
+
+	// do
+	req, _ := http.NewRequest("GET", "/qrcode?workplaceindex=0&date=2022-09-07", nil)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	// after
+	assert := assert.New(t)
+	var qr entity.Qrcode
+	json.Unmarshal(w.Body.Bytes(), &qr)
+
+	assert.Equal(0, qr.WorkplaceIndex)
+	assert.Equal("2022-09-07", qr.Date)
+	assert.Equal(1234, qr.RandomNum)
+}
+
+func TestSetLaborContract(t *testing.T) {
 
 }
 
 func TestGetLaborContract(t *testing.T) {
-
-}
-
-func TestSetLaborContract(t *testing.T) {
 
 }
 
