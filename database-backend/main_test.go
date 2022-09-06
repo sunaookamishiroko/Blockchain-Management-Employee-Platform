19de6a0a3e29796e91013f62f53757fcd13d9ea4
이서윤 <idfn3535@gmail.com>
2022-09-06 16:55:13 +0900

    Feat : setlaborcontract 테스트 코드 추가, qrcode 테스트 코드수정

--- database-backend/main_test.go
+++ database-backend/main_test.go
@@ -36,12 +36,11 @@ func TestSetQRcode(t *testing.T) {
 	db, _ := sql.Open("mysql", "root:rootroot@tcp(127.0.0.1:3306)/capstone")
 	defer db.Close()
 
+	var actual entity.Qrcode
 	db.QueryRow("SELECT * FROM qrcode WHERE workplaceindex= ? AND date= ?",
-		0, "2022-09-07").Scan(&qr.WorkplaceIndex, &qr.Date, &qr.RandomNum)
+		0, "2022-09-07").Scan(&actual.WorkplaceIndex, &actual.Date, &actual.RandomNum)
 
-	assert.Equal(0, qr.WorkplaceIndex)
-	assert.Equal("2022-09-07", qr.Date)
-	assert.Equal(1234, qr.RandomNum)
+	assert.Equal(qr, actual)
 }
 
 func TestGetQRcode(t *testing.T) {
@@ -64,17 +63,96 @@ func TestGetQRcode(t *testing.T) {
 	var qr entity.Qrcode
 	json.Unmarshal(w.Body.Bytes(), &qr)
 
-	assert.Equal(0, qr.WorkplaceIndex)
-	assert.Equal("2022-09-07", qr.Date)
-	assert.Equal(1234, qr.RandomNum)
+	expected := entity.Qrcode{
+		WorkplaceIndex: 0,
+		Date:           "2022-09-07",
+		RandomNum:      1234,
+	}
+	assert.Equal(expected, qr)
 }
 
 func TestSetLaborContract(t *testing.T) {
+	// before
+	router := gin.Default()
+	router.POST("/laborcontract", setLaborContract)
 
+	contract1 := entity.LaborContract{
+		Address:        "0x8F22cbB2Fe066d8671c9C09bfF005F0507e1627e",
+		WpName:         "GS25 한국공학대점",
+		WpEmployer:     "홍길동",
+		EmployeeName:   "이서윤",
+		WorkplaceIndex: 0,
+		Period:         "2022-04-28~2022-06-30",
+		Duties:         "서빙 재고정리",
+		WorkingTime:    "12:00-19:00",
+		WorkingDays:    "목금",
+		Wage:           "9160",
+		WageDay:        "매달 31일",
+		Comment:        "없음",
+	}
+	body1, _ := json.Marshal(contract1)
+
+	contract2 := entity.LaborContract{
+		Address:        "0x8F22cbB2Fe066d8671c9C09bfF005F0507e1627e",
+		WpName:         "올리브영 시화점",
+		WpEmployer:     "호잇호잇",
+		EmployeeName:   "이서윤",
+		WorkplaceIndex: 1,
+		Period:         "2022-03-01~2022-09-30",
+		Duties:         "계산",
+		WorkingTime:    "13:00-20:00",
+		WorkingDays:    "토일",
+		Wage:           "10000",
+		WageDay:        "매달 31일",
+		Comment:        "없음",
+	}
+	body2, _ := json.Marshal(contract2)
+
+	// do
+	req, _ := http.NewRequest("POST", "/laborcontract", bytes.NewBuffer(body1))
+	w1 := httptest.NewRecorder()
+	router.ServeHTTP(w1, req)
+
+	req, _ = http.NewRequest("POST", "/laborcontract", bytes.NewBuffer(body2))
+	w2 := httptest.NewRecorder()
+	router.ServeHTTP(w2, req)
+
+	// after
+	assert := assert.New(t)
+	db, _ := sql.Open("mysql", "root:rootroot@tcp(127.0.0.1:3306)/capstone")
+	defer db.Close()
+
+	rows, _ := db.Query("SELECT * FROM laborcontract WHERE address= ?",
+		"0x8F22cbB2Fe066d8671c9C09bfF005F0507e1627e")
+
+	contracts := make([]entity.LaborContract, 0)
+
+	for rows.Next() {
+		var contract entity.LaborContract
+		rows.Scan(
+			&contract.Address,
+			&contract.WpName,
+			&contract.WpEmployer,
+			&contract.EmployeeName,
+			&contract.WorkplaceIndex,
+			&contract.Period,
+			&contract.Duties,
+			&contract.WorkingTime,
+			&contract.WorkingDays,
+			&contract.Wage,
+			&contract.WageDay,
+			&contract.Comment,
+		)
+
+		contracts = append(contracts, contract)
+	}
+
+	assert.Equal(contract1, contracts[0])
+	assert.Equal(contract2, contracts[1])
 }
 
 func TestGetLaborContract(t *testing.T) {
-
+	// before
 }
 
 func TestDeleteLaborContract(t *testing.T) {
